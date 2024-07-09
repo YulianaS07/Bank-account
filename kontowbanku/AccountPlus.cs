b257acaed012aa095e3be780478e98b709f1ae34
yulia <yulia@YULLIA>
2024-07-09 14:22:09 +0200

    Add project files.

---
+++ kontowbanku/AccountPlus.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class AccountPlus : Account, IAccountWithLimit 
+    {
+        private decimal limit;
+        public decimal OneTimeDebetLimit 
+        {
+            get => limit;
+            set
+            {
+                if (!IsBlocked && value >= 0)
+                {
+                    limit = value;
+                    ZmianaAvaiableFounds();
+                }
+            }
+        }
+
+        public decimal AvaibleFounds { get; private set; }
+       
+
+        public AccountPlus(string name, decimal initialLimit = 100, decimal initialBalance = 0) : base(name, initialBalance)
+        {
+            if (initialLimit < 0)
+                initialLimit = 0;
+
+            limit = initialLimit;
+            ZmianaAvaiableFounds();
+        }
+        private void ZmianaAvaiableFounds()
+        {
+            AvaibleFounds = Balance + OneTimeDebetLimit;
+        }
+        public new bool Deposit(decimal amount)
+        {
+            if (base.Deposit(amount))
+            {
+                ZmianaAvaiableFounds();
+                Unblock1();
+                return true;
+            }
+            return false;
+        }
+        public new bool Withdrawal(decimal amount)
+        {
+            if (!IsBlocked && amount > 0 && AvaibleFounds >= amount)
+            {
+                base.Withdrawal(amount);
+                ZmianaAvaiableFounds();
+                if (Balance < 0)
+                {
+                    Block();
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public new void Block()
+        {
+            base.Block();
+            ZmianaAvaiableFounds();
+        }
+
+        public new void Unblock()
+        {
+            if (AvaibleFounds >= 0)
+            {
+                base.Unblock();
+                ZmianaAvaiableFounds();
+            }
+        }
+
+        private void Unblock1()
+        {
+            if (IsBlocked && AvaibleFounds >= 0)
+            {
+                base.Unblock();
+                ZmianaAvaiableFounds();
+            }
+        }
+
+
+        public override string ToString() =>
+            IsBlocked ? $"Account name: {Name}, balance: {Balance:F2}, blocked, avaible founds: {AvaibleFounds:F2}, limit: {OneTimeDebetLimit:F2}"
+                        : $"Account name: {Name}, balance: {Balance:F2}, avaible founds: {AvaibleFounds:F2}, limit: {OneTimeDebetLimit:F2}";
+
+
+    }
+}
+
+/*
+// kod C# 7.2 (kompilator Mono, Linux)
+// file: AccountPlus.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class AccountPlus : Account, IAccountWithLimit 
+    {
+        private decimal limit;
+        public decimal OneTimeDebetLimit 
+        {
+            get => limit;
+            set
+            {
+                if (!IsBlocked)
+                limit = Math.Round(value, PRECISION);
+            }
+        }
+
+        public decimal AvaibleFounds 
+        {
+            get => Balance + OneTimeDebetLimit;
+        }
+
+        public AccountPlus(string name, decimal initialLimit = 100, decimal initialBalance = 0) : base(name, initialBalance)
+        {
+            if (initialLimit < 0)
+                OneTimeDebetLimit = 0;
+            else
+                OneTimeDebetLimit =  Math.Round(initialLimit, PRECISION);
+        }
+
+        public override string ToString() =>
+            IsBlocked ? $"Account name: {Name}, balance: {Balance:F2}, blocked, avaible founds: {AvaibleFounds:F2}, limit: {OneTimeDebetLimit:F2}"
+                        : $"Account name: {Name}, balance: {Balance:F2}, avaible founds: {AvaibleFounds:F2}, limit: {OneTimeDebetLimit:F2}";
+
+
+    }
+}
+*/
---
+++ kontowbanku/Program.cs
@@ -0,0 +1,152 @@
+﻿using Bank;
+
+//Test1();
+//Test2();
+//Test3();
+//Test4();
+//Test5();
+//Test11();
+Test12();
+
+
+
+void Test1()
+{
+    /* wypłaty
+*/
+    var account = new Account("John");
+    account.Deposit(100.00m);
+    Console.WriteLine(account.Withdrawal(10.00m));
+    Console.WriteLine(account);
+    Console.WriteLine(account.Withdrawal(100.00m));
+    Console.WriteLine(account);
+    Console.WriteLine(account.Withdrawal(0.00m));
+    Console.WriteLine(account);
+    Console.WriteLine(account.Withdrawal(-10.00m));
+    Console.WriteLine(account);
+    account.Block();
+    Console.WriteLine(account.Withdrawal(10.4999m));
+    Console.WriteLine(account);
+}
+
+void Test2()
+{
+    /* Utworzenie konta dla dwóch argumentów, ujemne saldo początkowe
+*/
+    try
+    {
+        var account = new Account("Jim", -100.01m);
+        Console.WriteLine(account);
+    }
+    catch (ArgumentOutOfRangeException)
+    {
+        Console.WriteLine("negative argument");
+    }
+}
+
+void Test3()
+{
+    /* Utworzenie konta dla dwóch argumentów, nazwa jest null
+*/
+    try
+    {
+        var account = new Account(null, 100.0m);
+        Console.WriteLine(account);
+    }
+    catch (ArgumentOutOfRangeException)
+    {
+        Console.WriteLine("Name is null");
+    }
+}
+
+void Test4()
+{
+    /* wpłata, kwota ujemna, konto zablokowane,
+saldo nie zmienione */
+    var account = new Account("John");
+    account.Block();
+    Console.WriteLine(account.Deposit(-100.2345m));
+}
+
+void Test5()
+{
+    /* wypłaty
+*/
+    var account = new Account("John");
+    account.Deposit(100.00m);
+    Console.WriteLine(account.Withdrawal(10.00m));
+    Console.WriteLine(account);
+    Console.WriteLine(account.Withdrawal(100.00m));
+    Console.WriteLine(account);
+    Console.WriteLine(account.Withdrawal(0.00m));
+    Console.WriteLine(account);
+    Console.WriteLine(account.Withdrawal(-10.00m));
+    Console.WriteLine(account);
+    account.Block();
+    Console.WriteLine(account.Withdrawal(10.4999m));
+    Console.WriteLine(account);
+}
+
+void Test11()
+{
+    // tworzenie konta, zmiana limitu
+    // utworzenie konta plus z domyslnym limitem 100
+    var john = new AccountPlus("John");
+    Console.WriteLine(john);
+
+    // utworzenie konta plus z ustawionym limitem na 200 i bilansem początkowym 99
+    var eve = new AccountPlus("Eve", initialLimit: 200.0m, initialBalance: 99.0m);
+    Console.WriteLine(eve);
+
+    // zmiana limitu, konto nie zablokowane
+    eve.OneTimeDebetLimit = 120.0m;
+    Console.WriteLine(eve);
+
+    // próba zmiany limitu, konto zablokowane
+    eve.Block();
+    eve.OneTimeDebetLimit = 500.0m;
+    Console.WriteLine(eve);
+
+    // próba utworzenia konta z limitem ujemnym
+    var stan = new AccountPlus(name: "Stan", initialLimit: -200.0m);
+    Console.WriteLine(stan);
+
+
+}
+
+void Test12()
+{
+    // scenariusz: wpłaty wypłaty, blokada konta
+    // utworzenie konta plus z domyslnym limitem 100
+    var john = new AccountPlus("John", initialBalance: 100.0m);
+    Console.WriteLine(john);
+
+    // wypłata - podanie kwoty ujemnej
+    john.Withdrawal(-50.0m);
+    Console.WriteLine(john);
+
+    // wypłata bez wykorzystania debetu
+    john.Withdrawal(50.0m);
+    Console.WriteLine(john);
+
+    // wypłata z wykorzystaniem debetu
+    john.Withdrawal(100.0m);
+    Console.WriteLine(john);
+
+    // konto zablokowane, wypłata niemożliwa
+    john.Withdrawal(10.0m);
+    Console.WriteLine(john);
+
+    // wpłata odblokowująca konto
+    john.Deposit(80.0m);
+    Console.WriteLine(john);
+
+    // wpłata podanie kwoty ujemnej
+    john.Deposit(-80.0m);
+    Console.WriteLine(john);
+}
+
+
+
+
+
